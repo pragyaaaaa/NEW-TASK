1a35353f24723d2600f43167ef94b3b8e527e060
pragyaaaaa <pragyas0211@gmail.com>
2024-06-24 07:13:42 +0530

    bleh

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function toggleDropdown() {
       dropdown.style.display = "block";
   }
 }
+const button = document.getElementById('alertButton');
+button.addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'blue';
+});
+button.addEventListener('mouseout', function() {
+    this.style.backgroundColor = '';
+});
+
+const button2 = document.getElementById('announcementButton');
+button2.addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'blue';
+});
+button2.addEventListener('mouseout', function() {
+    this.style.backgroundColor = '';
+});
+
+const button3 = document.getElementById('accountButton');
+button3.addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'blue';
+});
+button3.addEventListener('mouseout', function() {
+    this.style.backgroundColor = '';
+});
