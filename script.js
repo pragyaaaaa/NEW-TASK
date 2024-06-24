c2c7862b47cb75cffa73399b754895eaf2ec73bf
pragyaaaaa <pragyas0211@gmail.com>
2024-06-24 07:28:54 +0530

    bleh

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleDropdown() {
 }
 const button = document.getElementById('alertButton');
 button.addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'blue';
+    this.style.backgroundColor = 'black';
 });
 button.addEventListener('mouseout', function() {
     this.style.backgroundColor = '';
@@ -16,7 +16,7 @@ button.addEventListener('mouseout', function() {
 
 const button2 = document.getElementById('announcementButton');
 button2.addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'blue';
+    this.style.backgroundColor = 'black';
 });
 button2.addEventListener('mouseout', function() {
     this.style.backgroundColor = '';
@@ -24,7 +24,7 @@ button2.addEventListener('mouseout', function() {
 
 const button3 = document.getElementById('accountButton');
 button3.addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'blue';
+    this.style.backgroundColor = 'black';
 });
 button3.addEventListener('mouseout', function() {
     this.style.backgroundColor = '';
